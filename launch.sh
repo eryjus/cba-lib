f9db788c60f6e6696cbb6026eb7899a653f6725d
Adam Clark <eryjus@users.noreply.github.com>
2019-08-28 13:06:51 -0700

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -8,10 +8,13 @@ then
     touch /var/run/mysqld/gitpod-init.lock
 
     # initialize database structures on disk, if needed
-    [ ! -d /workspace/mysql ] && mysqld
+    [ ! -d /workspace/mysql ] && mysqld --initialize-insecure
 
     # launch database, if not running
     [ ! -e /var/run/mysqld/mysqld.pid ] && mysqld --daemonize
+    
+    # secure the installation
+    [ ! -e /workspace/mysql/.secured ] && mysql_secure_installation && touch /workspace/mysql/.secured
 
     rm /var/run/mysqld/gitpod-init.lock
 fi
